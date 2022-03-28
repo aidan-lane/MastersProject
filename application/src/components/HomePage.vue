93aad913b8a2522bcfb2de07fce867276ed08afc
aidan-lane <aidanlane7@gmail.com>
2022-03-28 07:27:05 -0400

    Modified vue framework

---
+++ application/src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-text-field class="pt-5 mr-2" placeholder="Strawberries" outlined clearable></v-text-field>
+      <v-btn dark large color="pink"> SEARCH </v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HomePage',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
