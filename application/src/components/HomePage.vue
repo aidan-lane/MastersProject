6522924727c3803b6b479241fef934cb61eac1a0
aidan-lane <aidanlane7@gmail.com>
2022-03-28 18:22:09 -0400

    clean ui design

--- application/src/components/HomePage.vue
+++ application/src/components/HomePage.vue
@@ -1,41 +1,51 @@
 <template>
   <v-container
     class="container"
+    style="height: 90%; width: 100%;"
   >
-    <v-row
-      class="row"
+    <v-sheet
+      color="white"
+      elevation="5"
+      rounded="lg"
+      style="height: 100%; margin-left: 10%; margin-right: 10%;"
     >
-      <v-text-field
-        v-model="keyword"
-        label="Keyword" 
-        solo
-        @keydown.enter="onSubmit"
+      <v-row
+        class="row pt-10"
+        style="padding-left: 10%; padding-right: 10%;"
       >
-      </v-text-field>
-      <v-btn 
-        dark 
-        large
-        color="blue"
-        @click="onSubmit"
-        class="ml-2"
-        style="margin-top: 3px;"
-      > 
+        <v-text-field
+          v-model="keyword"
+          label="Keyword" 
+          filled
+          @keydown.enter="onSubmit"
+        >
+        </v-text-field>
+        <v-btn 
+          dark 
+          large
+          color="#4E73CC"
+          @click="onSubmit"
+          class="ml-2"
+          style="margin-top: 0.55%;"
+        > 
           Enter 
-      </v-btn>
-    </v-row>
-    <v-row
-      class="mt-0"
-    >
-      <v-chip 
-        v-for="word in keywords" :key="word"
-        close
-        close-icon="mdi-close-outline"
-        class="mr-1 ml-1 mt-1 mb-1"
-        @click:close="removeKeyword(word)"
+        </v-btn>
+      </v-row>
+      <v-row
+        class="mt-0"
+        style="padding-left: 10%; padding-right: 10%;"
       >
-        {{ word }}
-      </v-chip>
-    </v-row>
+        <v-chip 
+          v-for="word in keywords" :key="word"
+          close
+          close-icon="mdi-minus-circle"
+          class="mr-1 ml-1 mt-1 mb-1"
+          @click:close="removeKeyword(word)"
+        >
+          {{ word }}
+        </v-chip>
+      </v-row>
+    </v-sheet>
   </v-container>
 </template>
 
@@ -68,7 +78,7 @@
   }
 
   .row {
-    padding-left: 10%;
-    padding-right: 10%;
+    padding-left: 20%;
+    padding-right: 20%;
   }
 </style>
